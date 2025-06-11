4631d1fc5c4e161054de19a65b6b77551633490c
deryltazreva <32772076+deryltazreva@users.noreply.github.com>
2025-06-11 16:55:18 +0700

    update

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, request, jsonify
+import tensorflow as tf
 import joblib
 
 app = Flask(__name__)
 
-# Muat model
-model = joblib.load('model/tokenizer.pkl')
+# Load the model (make sure this points to the correct location)
+model = tf.keras.models.load_model('model/capstone.h5')
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get_json()
-    prediction = model.predict([data["input"]])
-    return jsonify({"prediction": prediction.tolist()})
+    try:
+        data = request.get_json()  # Get input data from request
+        input_data = data['input']
+        
+       
+        input_array = tf.convert_to_tensor(input_data)
+
+        # Make prediction
+        prediction = model.predict(input_array)
+        
+        return jsonify({'prediction': prediction.tolist()})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
-
